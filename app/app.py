d0176a4b8c25249e07e363fd47a056bfa8d1c38d
MikeDoes <40600201+MikeDoes@users.noreply.github.com>
2022-09-26 00:13:38 +0200

    added pages to the web app to explore patent

--- app/app.py
+++ app/app.py
@@ -7,3 +7,11 @@ static_folder='static',
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/search_results")
+def search_results():
+    return render_template('search_results.html')
+
+@app.route("/patent/<patent_no>")
+def display_patent(patent_no):
+    return render_template('patent.html', data=patent_no)
