7d72c2ff261a74ded4e7e8f3310e05fed4453584
MikeDoes <40600201+MikeDoes@users.noreply.github.com>
2022-09-25 16:42:42 +0200

    updated_xml_sample

--- epo/text_data_processing.py
+++ epo/text_data_processing.py
@@ -1,16 +1,23 @@
 import xml.etree.ElementTree as ET
+import os
+import json
+from tqdm import tqdm
+
 
 def open_xml_file(path="epo/samples/full-text/EP06818318NWB1/EP06818318NWB1.xml"):
     data = ET.parse(path).getroot()
     return data
 
+
 def create_average_file():
     """Is able to generate fictional patent like data-structures"""
     pass
 
+
 def create_max_file():
     """Renders the largest xml tree"""
     pass
+
 
 def print_tree_with_tags(xml_data, max_level=8):
     for child_1 in xml_data:
@@ -20,18 +27,52 @@ def print_tree_with_tags(xml_data, max_level=8):
             for child_3 in child_2:
                 print(f'\t\t#Child level 3: {child_3.tag}')
                 for child_4 in child_3:
-                    print(f'\t\t\t#Child level 4: {child_4.tag}')   
+                    print(f'\t\t\t#Child level 4: {child_4.tag}')
                     for child_5 in child_4:
                         print(f'\t\t\t\t#Child level 5: {child_5.tag}')
                         for child_6 in child_5:
-                            print(f'\t\t\t\t\t#Child level 6: {child_6.tag}')   
+                            print(f'\t\t\t\t\t#Child level 6: {child_6.tag}')
                             for child_7 in child_6:
-                                print(f'\t\t\t\t\t\t#Child level 7: {child_7.tag}')   
+                                print(
+                                    f'\t\t\t\t\t\t#Child level 7: {child_7.tag}')
                                 for child_8 in child_7:
-                                    print(f'\t\t\t\t\t\t\t#Child level 8: {child_8.tag}')
+                                    print(
+                                        f'\t\t\t\t\t\t\t#Child level 8: {child_8.tag}')
                                     for child_9 in child_8:
-                                        print(f'\t\t\t\t\t\t\t\t#Child level 9: {child_9.tag}')
+                                        print(
+                                            f'\t\t\t\t\t\t\t\t#Child level 9: {child_9.tag}')
                                         for child_10 in child_9:
-                                            print(f'\t\t\t\t\t\t\t\t\t#Child level 10: {child_10.tag}')  
+                                            print(
+                                                f'\t\t\t\t\t\t\t\t\t#Child level 10: {child_10.tag}')
 
-print_tree_with_tags(open_xml_file())
+
+def retrieve_section_details(xml_data, patent_id):
+    """Functions that returns the top level section names and their attributes. Returns in the form of array of dict. All in memory function"""
+    details_dict = []
+    for el in xml_data:
+        attrib_dict = {attrib:value for attrib, value in el.attrib.items()}
+        details_dict += [{"patent_no": patent_id, "id": xml_data.attrib['id'], "tag": el.tag, **attrib_dict}]
+    return details_dict
+
+
+def retrieve_all_section_details_folder(input_dir_path):
+    combined_list = []
+    for i, file in enumerate(os.listdir(input_dir_path)):
+        if file.endswith(".xml") and not file.startswith('TOC.xml') and not file.startswith(".DS"):
+            data = open_xml_file(f"{input_dir_path}/{file}")
+            combined_list += retrieve_section_details(data, patent_id=input_dir_path.split('/')[-1])
+    return combined_list
+
+
+data = []
+for batch in tqdm(os.listdir(f"data/DOC-UNPACKED")):
+    batch_level_data = []
+
+    for patent in os.listdir(f"data/DOC-UNPACKED/{batch}"):
+        batch_level_data += retrieve_all_section_details_folder(
+            f"data/DOC-UNPACKED/{batch}/{patent}")
+
+    data += batch_level_data
+
+with open("section_summary.json", "w") as f:
+    json.dump(data, f)
