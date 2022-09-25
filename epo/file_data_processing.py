23cdce615852f6e2603d0706d5df526e8d7f5630
MikeDoes <40600201+MikeDoes@users.noreply.github.com>
2022-09-25 15:20:02 +0200

    seperated image processing

--- epo/file_data_processing.py
+++ epo/file_data_processing.py
@@ -24,19 +24,4 @@ def unzip_all_subfolders(input_dir_path="data/DOC", output_dir_path="data/DOC-UN
     for folder in tqdm(os.listdir(input_dir_path)):
         os.mkdir(f"{output_dir_path}/{folder}")
         unzip_all_folders(input_dir_path=f"{input_dir_path}/{folder}", output_dir_path= f"{output_dir_path}/{folder}", limit=99999)
-
-
-# Image Conversions
-
-def convert_tiff_to_png(tiff_import_path='image.tif', png_export_path="image.png"):
-    """Converts image using the PILLOW library from tif to png"""
-    image = Image.open(tiff_import_path)
-    return image.save(png_export_path, format="png")
-
-def convert_subfolder_images(input_dir_path="epo/samples/full-text", output_dir_path="static/img/patent_images"):
-    """Converts all the images in a directory which contains a folders that in turn contain tif images"""
-    for folder in os.listdir(input_dir_path):
-        for file in os.listdir(f'{input_dir_path}/{folder}'):
-            if file.endswith('.tif'):
-                if file.startswith('imgf'):
-                    convert_tiff_to_png(f'{input_dir_path}/{folder}/{file}', f'{output_dir_path}/{file.replace(".tif", "")}{folder}.png')
+        
