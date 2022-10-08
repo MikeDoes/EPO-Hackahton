37819d020afdbe6be299832599c8ab2f731d746c
MikeDoes <40600201+MikeDoes@users.noreply.github.com>
2022-10-08 22:24:45 +0200

    Working on create good hierchical CPC scheme. Need to create sub class logics

--- epo/cpc_data_processing.py
+++ epo/cpc_data_processing.py
@@ -1,5 +1,6 @@
 ### Library that deals with the processing of CPC data from https://www.cooperativepatentclassification.org/cpcSchemeAndDefinitions/bulk ###
 import csv
+import xml.etree.cElementTree as ET
 
 def open_tsv_file(path=""):
     """Opens TSV file"""
@@ -11,9 +12,48 @@ def open_tsv_file(path=""):
     return table
 
 def export_xml_file(data, output_path="data/E_test.xml"):
+    
+    
+    root = ET.Element('section', attrib={
+        "code": data[0][0],
+        "title": data[0][2],
+    })
+    
+    nest_counter = 0
+    
+    for row in data[1:]:
+        if len(row[0]) == 3:
+            # This is a main group
+            main_group = ET.SubElement(root, 'main_group', attrib={
+                "code": row[0],
+                "title": row[2]
+            })
+
+        if len(row[0]) == 4:
+            # This is a sub group
+            sub_group = ET.SubElement(main_group, 'sub_group', attrib={
+                "code": row[0],
+                "title": row[2]
+            })
+
+        if len(row[0]) > 4:
+            # This is a class or sub-class. Got to review this nested logic
+            if int(row[1]) == 0:
+                main_class = ET.SubElement(sub_group, 'main_class', attrib={
+                    "code": row[0],
+                    "title": row[2]
+                })
+                nest_counter = 0
+            
+            if int(row[1]) == 1:
+                classification_class = ET.SubElement(main_class, 'sub_class', attrib={
+                    "code": row[0],
+                    "title": row[2]
+                })
+                nest_counter = 1
 
 
-data = open_tsv_file('data/CPCTitleList202208/cpc-section-E_20220801.txt')
+    return ET.ElementTree(root)
 
-
-for i, data in open_tsv_file('')
+a = export_xml_file(open_tsv_file('data/CPCTitleList202208/cpc-section-E_20220801.txt'))
+a.write("filename.xml")
